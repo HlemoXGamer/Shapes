6c1487fcb474b203a332fff8f623b3bff261146a
YoussefHlem <80886022+HlemoXGamer@users.noreply.github.com>
2023-06-11 13:10:25 +0300

    Done

---
+++ shape-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Shape from "./components/Shape/Shape";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Shape />
+    </div>
+  );
+};
+
+export default App;
---
+++ shape-project/src/components/UploadC/UploadC.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UploadC = () => {
+  return <div>UploadC</div>;
+};
+
+export default UploadC;
